8afd723728b5b5fcddbeda853662d4e0ba57b5e0
Tschebee <babsi.joebstl@gmail.com>
2017-01-15 19:44:26 +0100

    sunday commit

---
+++ 19_UnrealWebcamFun/unreal.js
@@ -0,0 +1,18 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({
+    video: true;
+    audio: false;
+  }).then(localMediaStream => {
+    //convert video stream for our player
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  });
+
+  getVideo();
+}
